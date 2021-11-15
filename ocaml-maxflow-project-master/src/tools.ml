ee00e0d55634c51e174709d173dad1731700da5f
marietousarr <marietou60@gmail.com>
2021-11-15 18:29:36 +0100

    programmation clone et gmap

--- ocaml-maxflow-project-master/src/tools.ml
+++ ocaml-maxflow-project-master/src/tools.ml
@@ -2,22 +2,7 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes (gr:'a graph) = 
-  let rec loop graphacc mygr = match mygr with 
-    |[]-> graphacc
-    |(id, _):: rest -> loop (new_node mygraph id) rest
-  in loop (empty_graph) gr
+let clone_nodes (gr:'a graph) = n_fold gr new_node empty_graph
 
-(*
-let gmap gr f = 
-  let mygraph = empty_graph
-  in 
-    let rec loop mygr = match mygr with 
-      |[]-> mygraph
-      |(id1, out_arcs):: rest -> match out_arcs with
-                        |[] -> loop rest
-                        |(id2, lbl)::rest2 -> (new_arc mygraph id1 id2 (f lbl)); 
 
-    in loop gr
-
-    *)
+let gmap gr f = e_fold gr (fun gr id1 id2 lbl ->  new_arc gr id1 id2 (f lbl)) (clone_nodes gr)
